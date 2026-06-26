e857dc570606ebb014f3dbff3d85398a8b0c8b5a
Akshay Shrivastava <73992286+akshim1702@users.noreply.github.com>
2026-06-27 00:14:48 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length !== t.length) return false;
+        const hash = {};
+        for(let i=0; i<s.length;i++){
+           if(hash[s[i]]){
+            hash[s[i]] += 1;
+           }else{
+            hash[s[i]] = 1;
+           }
+        }
+
+        for (let j=0; j<t.length;j++){
+            if(hash[t[j]]){
+                hash[t[j]] -=1;
+                if(hash[t[j]] == 0){
+                    delete hash[t[j]];
+                }
+            }
+        }
+
+        return Object.keys(hash).length === 0;
+    }
+}
